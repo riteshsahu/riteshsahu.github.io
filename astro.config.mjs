920a28cac1e39820c29eced535d36db3e5143e52
Ritesh Sahu <riteshsahu314@gmail.com>
2024-08-22 02:18:35 +0530

    add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   site: "https://riteshsahu.github.io",
   output: "static",
 });
